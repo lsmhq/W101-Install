ee53c765bd68a195d5398e3d86071dba31c41c70
daitianfang <784433957@qq.com>
2022-08-05 16:46:35 +0800

    终极版本

--- install_zh_cn.js
+++ install_zh_cn.js
@@ -1,7 +1,15 @@
 const fs = require('fs')
 const request = require('request')
+const child = require('child_process')
 var ProgressBar = require("progress");
 const readline = require('readline');
+// const printDouble = require('console-png');
+// require('console-png').attachTo(console);
+// let logo = 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-479328cb-417a-467c-9512-83793cb72c1e/e9b191af-1eb1-4f63-9270-3a27b2938704.png'
+// var image 
+// getFile(logo, './logo.png',()=>{
+//     image = fs.readFileSync('./logo.png');
+// }, false)
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -9,8 +17,8 @@ const rl = readline.createInterface({
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 const args = process.argv.slice(2)
-let path = './' // 打包路径
-// let path = __dirname // 本地路径
+let path = './Wizard101/Data/GameData/' // 打包路径
+// let path = './' // 本地路径
 let downLoadArr = ['d', 'r', 'c']
 let params = {
     r:'release',
@@ -38,8 +46,11 @@ if(type === 'help'){
     -d:   检测最新测试版并安装\r\n
     -u:   检测最新一键安装并更新(暂无❌)\r\n
     -i:   初始化\r\n
+    -p:   快速螺旋启动\r\n
     -c:   聊天纯享版下载\r\n
+    -lsmhq彩蛋\r\n
     感谢阅读！`);
+    console.png(image);
     process.exit();
 }else if(downLoadArr.includes(type)){
     downLoad()
@@ -106,7 +117,7 @@ function downLoad(){
       })
 }
 // 下载文件
-function getFile(uri, filePath, callback){
+function getFile(uri, filePath, callback, showProgress = true){
   if (uri) {
    let currentTotal = 0 
    let total = 0
@@ -118,20 +129,25 @@ function getFile(uri, filePath, callback){
     total = res.headers['content-length']
    })
    req.on('data',(data)=>{
-    currentTotal += data.byteLength
-    // console.log(currentTotal)
-    var bar = new ProgressBar(`[ :bar ]${(currentTotal/1024/1024).toFixed(2)}MB/${(total/1024/1024).toFixed(2)}MB`, { total: total*1, curr:currentTotal });
-    bar.tick();
-    if (bar.complete) {
-        out.close()
-        logColor("❤下载完成!\n");
-        // logColor(``,33)
+    if(showProgress){
+        currentTotal += data.byteLength
+        // console.log(currentTotal)
+        var bar = new ProgressBar(`[ :bar ]${(currentTotal/1024/1024).toFixed(2)}MB/${(total/1024/1024).toFixed(2)}MB`, { total: total*1, curr:currentTotal });
+        bar.tick();
+        if (bar.complete) {
+            out.close()
+            logColor("❤下载完成!\n");
+            // logColor(``,33)
+            callback(filePath)
+        }
+    }else{
         callback(filePath)
     }
+
    })
   }
 }
-
+// 继续？
 function next(){
     rl.question(`${changeColor(`是否继续安装输入yes(y)或者no(n)并回车确认? `, 96, 4)}\r\n`,(yon)=>{
         // console.log(yon.toLocaleLowerCase())
@@ -148,14 +164,33 @@ function next(){
         // console.clear()
     })
 }
+// 回答
 function question(){
     rl.question(`
-${changeColor('测试版 (d/D)',93)} ${changeColor('正式稳定版 (r/R)', 94)} ${changeColor('聊天纯享版 (c/C)')} ${changeColor('重置 (i/I)', 91)}
+${changeColor('安装测试版 (d/D)',93)} ${changeColor('安装正式稳定版 (r/R)', 94)} ${changeColor('安装聊天纯享版 (c/C)')} ${changeColor('快速螺旋启动 (p/P)',96)} ${changeColor('重置 (i/I)', 91)} 帮助 (h/H)
 
-${changeColor(`输入补丁对应的英文字母并回车确认:`, 96, 4)}`, name => {
+${changeColor(`输入操作对应的英文字母并回车确认:`, 96, 4)}`, name => {
     let arr = ['r','c','i','d']
                 type = name.toLocaleLowerCase()
                 // console.log('out', name)
+                if(type == 'p'){
+                    let exe = './Wizard101/Bin/WizardGraphicalClient.exe -L login.us.wizard101.com 12000'
+                    exe = './release/向日葵.lnk'
+                    // console.log(exe)
+                    child.exec(`"${exe}"`,(err, stdout, stderr)=>{
+                        if(err) 
+                            return console.error('出现了某些错误但,不必管他.jpg');
+                        // 返回结果封装在 stdout 中，字符串格式
+                        // console.log('123123',stdout.toString('utf8'));
+                    })
+                    question()
+                    return
+                }
+                if(type == 'h'){
+                    help()
+                    question()
+                    return
+                }
                 if(!arr.includes(type)){
                     question()
                     return
@@ -168,7 +203,7 @@ ${changeColor(`输入补丁对应的英文字母并回车确认:`, 96, 4)}`, nam
                 // console.clear()
           });
 }
-
+// 改变type
 function changeType(){
     let files = fs.readdirSync(path,{withFileTypes:true})
     let names = files.map(file=>file.name)
@@ -182,6 +217,7 @@ function changeType(){
         logColor("❤切换补丁完成，请重启游戏进行体验!\n");
     }
 }
+// 初始化
 function init(){
     let files = fs.readdirSync(path,{withFileTypes:true})
     let unlinkArr = ['version_zh_cn_d', 'version_zh_cn_r', 'version_zh_cn_u', 'version_zh_cn_c','Locale_English-Root.wad','Locale_English-Root.wad.d','Locale_English-Root.wad.r','Locale_English-Root.wad.c']
@@ -192,6 +228,18 @@ function init(){
     })
     logColor('\n重置完成！请重新安装...')
     question()
+}
+function help(){
+    // console.png(image);
+    console.log(`
+    欢迎来到使用说明指引\r\n               
+    下面是基本的操作说明:\r\n              
+    h:   召唤灭火器进行讲解\r\n
+    r:   检测最新正式版并安装\r\n
+    d:   检测最新测试版并安装\r\n
+    i:   初始化，删除所有补丁，谨慎操作\r\n
+    c:   聊天纯享版下载\r\n
+    p:   快速螺旋启动\r\n`);
 }
 // 版本号对比
 function compareVersion(v1, v2) {
