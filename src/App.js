349071e0cba9e21f723293643071f11e44107d42
daifang <784433957@qq.com>
2022-09-09 00:59:33 +0800

    111

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
 import Main from './page/main';
 function App() {
+  useEffect(() => {
+    document.onkeydown = function(){
+      var e = window.event || arguments[0];
+      if(e.keyCode === 123){    //屏蔽F12
+          return false;
+      }else if(e.ctrlKey && e.shiftKey && e.keyCode === 73){    //屏蔽Ctrl+Shift+I，等同于F12
+          return false;
+      }else if(e.shiftKey && e.keyCode === 121){    //屏蔽Shift+F10，等同于鼠标右键
+          return false;
+      }
+  }
+  }, [])
   return (
     <div className="App">
       <Main/>
