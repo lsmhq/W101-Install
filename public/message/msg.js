8998afd9e8f86126bfa379afa33c429aca8190d2
daitianfang <784433957@qq.com>
2022-09-13 17:44:56 +0800

    IIFE

--- public/message/msg.js
+++ public/message/msg.js
@@ -1,30 +1,30 @@
-const { ipcRenderer, shell, screen } = require('electron')
-
-// console.log(remote.app.getPath('exe'))
-
-
-function openB(url){
-    shell.openExternal(url);
-}
-function sound(){
-    shell.beep()
-}
-window.electronAPI = {
-    openGame: (title) => ipcRenderer.send('openGame', title),
-    downLoadFile:(type)=>ipcRenderer.send('downLoadFile',type),
-    openBroswer:openB,
-    sendXY:(e)=>{ipcRenderer.send('move-application',{posX:e.x,posY:e.y})},
-    mini:()=>{ipcRenderer.send('mini')},
-    close:()=>{ipcRenderer.send('close')},
-    getScale:()=>{ipcRenderer.on('scale',(event, scale)=>{
-        console.log(scale)
-        document.body.style.zoom = 1 + (1 - scale)
-        localStorage.setItem('scale', scale)
-    })},
-    sound,
-    getScaleFactor:()=>{
-        // let scaleFactor = screen.getAllDisplays()[0].scaleFactor
-        console.log(screen)
-        // return scaleFactor
+(function(){
+    const { ipcRenderer, shell, screen } = require('electron')
+    
+    function openB(url){
+        shell.openExternal(url);
     }
-}
+    function sound(){
+        shell.beep()
+    }
+    window.electronAPI = {
+        openGame: (title) => ipcRenderer.send('openGame', title),
+        downLoadFile:(type)=>ipcRenderer.send('downLoadFile',type),
+        openBroswer:openB,
+        sendXY:(e)=>{ipcRenderer.send('move-application',{posX:e.x,posY:e.y})},
+        mini:()=>{ipcRenderer.send('mini')},
+        close:()=>{ipcRenderer.send('close')},
+        getScale:()=>{ipcRenderer.on('scale',(event, scale)=>{
+            console.log(scale)
+            document.body.style.zoom = 1 + (1 - scale)
+            localStorage.setItem('scale', scale)
+        })},
+        sound,
+        getScaleFactor:()=>{
+            // let scaleFactor = screen.getAllDisplays()[0].scaleFactor
+            console.log(screen)
+            // return scaleFactor
+        }
+    }
+})()
+
