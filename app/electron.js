7fb8bac3eeb79a79f2b3f39aec2763837a555890
daitianfang <784433957@qq.com>
2022-10-19 17:07:02 +0800

    kill exe

--- app/electron.js
+++ app/electron.js
@@ -1,3 +1,4 @@
+const { ChildProcess } = require('child_process');
 const { app, BrowserWindow, nativeImage, ipcMain, screen, Tray, Menu, ipcRenderer, shell } = require('electron');
 const { autoUpdater } = require('electron-updater'); 
 let mainWindow, loading, tray, width = 1250, height = 700
@@ -68,7 +69,6 @@ function createWindow () {
     mainWindow.setMaximumSize(parseInt(width/scaleFactor), parseInt(height/scaleFactor))
     mainWindow.setSize(parseInt(width/scaleFactor), parseInt(height/scaleFactor))
   })
-
   // 当窗口关闭时发出。在你收到这个事件后，你应该删除对窗口的引用，并避免再使用它。
   mainWindow.on('closed', () => {
     mainWindow = null;
@@ -76,6 +76,7 @@ function createWindow () {
     mainWindow = null;
     tray && tray.destroy()
     tray = null
+    killExe('launchWizard101.exe')
     // newWin = null
 
   });
@@ -306,4 +307,27 @@ if (!gotTheLock) {
 
         }
     })
+}
+
+
+function killExe (name) {
+  // process 不用引入，nodeJS 自带
+  // 带有命令行的list进程命令是：“cmd.exe /c wmic process list full”
+  //  tasklist 是没有带命令行参数的。可以把这两个命令再cmd里面执行一下看一下效果
+  // 注意：命令行获取的都带有换行符，获取之后需要更换换行符。可以执行配合这个使用 str.replace(/[\r\n]/g,""); 去除回车换行符 
+  let cmd = process.platform === 'win32' ? 'tasklist' : 'ps aux'
+  ChildProcess.exec(cmd, function (err, stdout, stderr) {
+      if (err) {
+          return console.error(err)
+      }
+      // console.log(stdout)
+      stdout.split('\n').forEach((line) => {
+          let processMessage = line.trim().split(/\s+/)
+          let processName = processMessage[0] //processMessage[0]进程名称 ， processMessage[1]进程id
+          if (processName === name) {
+              console.log('Kill Process---->', processMessage[1], processMessage)
+              process.kill(processMessage[1])
+          }
+      })
+  })
 }
