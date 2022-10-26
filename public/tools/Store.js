3f46a412e344620583b36c6228464e8026ce556f
daitianfang <784433957@qq.com>
2022-10-26 17:58:24 +0800

    11

--- public/tools/Store.js
+++ public/tools/Store.js
@@ -1,31 +1,31 @@
-(function(){
-    const Store = require('electron-store');
-    const  store = new Store();
-    function getPath(){
-        return store.path
-    }
-    function setItem(key, value){
-        store.set(key, value)
-    }
+// (function(){
+//     const Store = require('electron-store');
+//     const  store = new Store();
+//     function getPath(){
+//         return store.path
+//     }
+//     function setItem(key, value){
+//         store.set(key, value)
+//     }
 
-    function getItem(key){
-        return store.get(key)
-    }
+//     function getItem(key){
+//         return store.get(key)
+//     }
 
-    function hasItem(key){
-        return store.has(key)
-    }
+//     function hasItem(key){
+//         return store.has(key)
+//     }
 
-    function clear(key){
-        if(key){
-            store.delete(key)
-        }else{
-            store.clear()
-        }
-    }
+//     function clear(key){
+//         if(key){
+//             store.delete(key)
+//         }else{
+//             store.clear()
+//         }
+//     }
     
-    console.log('存储位置----->', getPath())
-    window.store = {
-        setItem, getItem, clear, hasItem, getPath
-    }
-})()
+//     console.log('存储位置----->', getPath())
+//     window.store = {
+//         setItem, getItem, clear, hasItem, getPath
+//     }
+// })()
