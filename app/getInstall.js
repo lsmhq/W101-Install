39513c5e8ef65138acb002bcd9ebee96d444b066
daitianfang <784433957@qq.com>
2022-12-16 17:22:42 +0800

    111

--- app/getInstall.js
+++ app/getInstall.js
@@ -1,5 +1,5 @@
-let path = 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\'
+let path = 'HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths'
 const regedit = require('regedit');
 regedit.list([path]).on('data', function (res) {
-    console.log(res)
+    console.log(res.data.keys)
 })
